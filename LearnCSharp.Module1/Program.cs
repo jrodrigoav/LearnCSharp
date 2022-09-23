c0c1e5d1b886de4ae524a5980abcb712540443d6
Jesus Acedo <jrodrigoav@gmail.com>
2022-09-22 20:13:12 -0500

    Completed Ejercicio1

--- LearnCSharp.Module1/Program.cs
+++ LearnCSharp.Module1/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
+app.MapGet("/", () => "Facbook sucks");
+app.MapGet("/suma", () => 1 + 1);
+Console.WriteLine("Hola mundo mundial");
+//app.Run();
